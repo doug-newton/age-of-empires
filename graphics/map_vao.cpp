6d1f15c406b94f834f674570b2b8ac80b931ed2a
Doug <27059467+doug-newton@users.noreply.github.com>
2024-01-27 19:02:39 +0200

    copy code from sprite to map vao

--- graphics/map_vao.cpp
+++ graphics/map_vao.cpp
@@ -12,34 +12,22 @@ namespace aoe_engine {
 		glGenVertexArrays(1, &this->m_id);
 		glBindVertexArray(this->m_id);
 
-		GLfloat positions[] = {
-			-0.5f, -0.5f, 1.0f,
-			 0.5f, -0.5f, 1.0f,
-			 0.5f,  0.5f, 1.0f,
-			-0.5f,  0.5f, 1.0f
+		GLfloat vertices[] = {
+			-0.5f, -0.5f, 1.0f,		0.0f, 0.0f,
+			 0.5f, -0.5f, 1.0f,		1.0f, 0.0f,
+			 0.5f,  0.5f, 1.0f,		1.0f, 1.0f,
+			-0.5f,  0.5f, 1.0f,		0.0f, 1.0f
 		};
 
-		GLuint tile_coords[] = {
-			0, 0,
-			0, 0,
-			0, 0,
-			0, 0
-		};
+		GLuint vbo;
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-		GLuint positions_vbo;
-		glGenBuffers(1, &positions_vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, positions_vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(positions), positions, GL_STATIC_DRAW);
-
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)0);
 		glEnableVertexAttribArray(0);
 
-		GLuint tile_coords_vbo;
-		glGenBuffers(1, &tile_coords_vbo);
-		glBindBuffer(GL_ARRAY_BUFFER, tile_coords_vbo);
-		glBufferData(GL_ARRAY_BUFFER, sizeof(tile_coords), tile_coords, GL_STATIC_DRAW);
-
-		glVertexAttribPointer(1, 2, GL_UNSIGNED_INT, GL_FALSE, 2 * sizeof(GLuint), (GLvoid*)(0));
+		glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
 		glEnableVertexAttribArray(1);
 
 		GLuint elements[] = {
