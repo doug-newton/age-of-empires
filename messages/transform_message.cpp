42fb8e92dc7d00d0f9021139c637bbd7eaff836a
Doug <27059467+doug-newton@users.noreply.github.com>
2024-02-01 07:47:55 +0200

    make note for next change

--- messages/transform_message.cpp
+++ messages/transform_message.cpp
@@ -1,6 +1,16 @@
 #include "transform_message.h"
 #include "../components/transform_component.h"
 
+/*
+	instead of messages being visitors, would make more sense for messages 
+	to accept components and for components to visit messages, i.e. base component
+	could have virtual 'onTransformMessage()', 'onModelMatrixMessage()', etc, and choose
+	whether or not to interact with / listen to these messages as required
+
+	too much TransformationComponent logic in the TransformMessage
+	- this logic should stay in the TransformComponent
+*/
+
 namespace aoe_engine {
 
 	TransformMessage::TransformMessage(void* sender, const glm::vec2& velocity, float r_velocity) :
